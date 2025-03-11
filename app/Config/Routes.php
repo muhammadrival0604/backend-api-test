2e86a37f18b2cfb3cba16784be0a6921bd89d7c1
muhammadrival06 <menangeleh123@gmail.com>
2025-03-11 09:06:31 +0700

    Membuat routing API

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,16 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->group('api', function ($routes) {
+    $routes->post('login', 'Api\AuthController::login');
+
+    $routes->get('users', 'Api\UserController::index');
+    $routes->get('users/(:num)', 'Api\UserController::show/$1');
+    $routes->post('users', 'Api\UserController::create');
+    $routes->put('users/(:num)', 'Api\UserController::update/$1');
+    $routes->delete('users/(:num)', 'Api\UserController::delete/$1');
+
+    $routes->get('search/name/(:any)', 'Api\UserController::searchByName/$1');
+    $routes->get('search/nim/(:num)', 'Api\UserController::searchByNIM/$1');
+    $routes->get('search/ymd/(:num)', 'Api\UserController::searchByYMD/$1');
+});
